51f86acd8c213496fff79aca4eb0a6e906b33a6c
NingLuo <luo.n@husky.neu.edu>
2018-06-01 12:33:11 -0500

    Add feature to delete recipe and cancel recipe creation;

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
@@ -13,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editMode: boolean = false;
   recipeForm: FormGroup;
 
-  constructor(private route:ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route:ActivatedRoute, private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -31,6 +31,8 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+    this.router.navigate(["../"], {relativeTo: this.route});
   }
 
   private InitForm(){
@@ -71,5 +73,10 @@ export class RecipeEditComponent implements OnInit {
         'amount': new FormControl(null, Validators.required)
       })
     );
+  }
+
+  onCancel(){
+    this.recipeForm.reset();
+    this.router.navigate(["../"], {relativeTo: this.route});
   }
 }
